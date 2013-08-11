e34146622a0286b22bec359be6444832800ddd1f
nullproduction <mezhevikin@gmail.com>
2013-08-11 14:52:24 +0400

    Initial Commit

---
+++ ParserPodcastsCategories/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ParserPodcastsCategories
+//
+//  Created by Администратор on 8/11/13.
+//  Copyright (c) 2013 Администратор. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ParserPodcastsCategories/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ParserPodcastsCategories
+//
+//  Created by Администратор on 8/11/13.
+//  Copyright (c) 2013 Администратор. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
